4893edc1c647a1c6d11cc5fb9dd42ddc2329e50b
Biruk Mintesnot <birukmintesnot1993@gmail.com>
2023-11-26 11:20:01 +0300

    BMI task

---
+++ js_new.js
@@ -0,0 +1,12 @@
+// console.log(age);
+let age = 25 ;
+console.log(age);
+// block  is the function with in clry barcet{} on (let) and(cons) 
+//var redeclartion
+if(age == 25){
+    let age='empty'
+
+
+}
+console.log(age)
+// is equal to "==" but "=== -check the type so js mostly used"
